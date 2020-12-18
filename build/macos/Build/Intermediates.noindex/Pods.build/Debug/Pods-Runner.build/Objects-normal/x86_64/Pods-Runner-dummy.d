ca657f5199d00e1de2017d2a42896f1b0b6866d8
omar <omars@php.net>
2020-12-18 11:08:49 +0100

    rest client: GET Album

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/omar/workspace/dart/ektebly/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/omar/workspace/dart/ektebly/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/omar/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/omar/workspace/dart/ektebly"
+export "FLUTTER_TARGET=/Users/omar/workspace/dart/ektebly/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Users/omar/development/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/omar/workspace/dart/ektebly/.dart_tool/package_config.json"
